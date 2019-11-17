ea4756de1676ce13b5a1db4f76c5695c43f14faa
akash <akash.testvagrant@embibe.com>
2019-11-17 13:03:42 +0530

    add test case for api

---
+++ src/test/java/ApiTestCases/TestApi.java
@@ -0,0 +1,42 @@
+package ApiTestCases;
+
+import base.TestBase;
+import io.restassured.response.ResponseBody;
+import utils.Properties;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class TestApi extends TestBase {
+    public List<String> listOfPhoneNumber;
+    private String userPhoneNumber;
+
+
+    @org.testng.annotations.Test
+    public void getUsers() {
+        ResponseBody userData = given().
+                header("Authorization", "Bearer " + token).
+                when().get(Properties.userRecordEndPoint).andReturn().then().
+                statusCode(200).extract().response().getBody();
+        listOfPhoneNumber = userData.jsonPath().getList("phone");
+        userPhoneNumber = userData.jsonPath().getList("phone").get(0).toString();
+
+
+    }
+
+
+    @org.testng.annotations.Test(dependsOnMethods = "getUsers")
+    public void getRecordOfUser() {
+        ResponseBody userData = given().header("Authorization", "Bearer " + token).when().get(String.format(Properties.userRecordByPhoneEndPoint, userPhoneNumber)).andReturn().then().
+                statusCode(200).extract().response().getBody();
+        userData.prettyPrint();
+
+    }
+
+
+}
+
+
+
+
